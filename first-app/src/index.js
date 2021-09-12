ffba81a59b7b39618ec15104d3646740597e868f
Marharyta Malets <Marharyta-Malets@epam.com>
2021-09-12 16:17:15 +0300

    feat: add Counter component

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const App = React.createElement('h1', null, 'Hello React!');
-
 ReactDOM.render(
-    App,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
