53378f5085b9bc4a55bd04542fbb37e6623a18cb
Marharyta Malets <Marharyta-Malets@epam.com>
2021-09-27 09:45:45 +0300

    fix: returned hover effect and cursor pointer

--- home-task-react-v1/src/components/Header/index.js
+++ home-task-react-v1/src/components/Header/index.js
@@ -6,7 +6,6 @@ import './index.css'
 const Header = () => {
     return (
         <header className='header wrapper'>
-            <img className='header_background' src='/images/background-header.jpg' alt='background' />
             <LogoIcon />
             <button className='header_add-movie'>
                 + add movie
