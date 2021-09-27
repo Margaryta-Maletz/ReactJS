0413e7a5cc0b104a66aaf936b637309beb831635
Marharyta Malets <Marharyta-Malets@epam.com>
2021-09-27 10:01:02 +0300

    fix: background image

--- home-task-react-v1/src/components/Header/index.js
+++ home-task-react-v1/src/components/Header/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import LogoIcon from '../LogoIcon';
 import { SearchInput } from '../SearchInput';
 import './index.css'
+import backgroundImage from '../../assets/background-header.jpg'
 
 const Header = () => {
+    const backgroundStyle = {
+        backgroundImage: 'url(' + backgroundImage + ')',
+    }
+
     return (
-        <header className='header wrapper'>
+        <header className='header wrapper' style={ backgroundStyle }>
             <LogoIcon />
             <button className='header_add-movie'>
                 + add movie
